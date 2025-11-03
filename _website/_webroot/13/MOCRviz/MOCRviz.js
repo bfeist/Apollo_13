64905a829619e1d1797228cee83558054f6ccac7
Ben Feist <benjamin.f.feist@nasa.gov>
2025-11-02 23:58:58 -0500

    New media source

--- _website/_webroot/13/MOCRviz/MOCRviz.js
+++ _website/_webroot/13/MOCRviz/MOCRviz.js
@@ -2,10 +2,10 @@ var cMissionDurationSeconds = 547200; //152 hours
 var cCountdownSeconds = 127048;
 var cAppStartGET = -102;
 
-// var cTapeCdnRoot = 'https://media.apolloinrealtime.org/A13/MOCR_audio';
+var cTapeCdnRoot = "https://media.apolloinrealtime.org/A13/MOCR_audio";
 // var cTapeCdnRoot = 'https://keycdnmedia.apolloinrealtime.org/A13/MOCR_audio'; //keycdn pulling from dreamhost
 // var cTapeCdnRoot = "https://keycdnmediado.apolloinrealtime.org/A13/MOCR_audio"; //keycdn pulling from digitalocean space
-var cTapeCdnRoot = "https://apolloinrealtimemedia.nyc3.digitaloceanspaces.com/A13/MOCR_audio"; //digitalocean space
+// var cTapeCdnRoot = "https://apolloinrealtimemedia.nyc3.digitaloceanspaces.com/A13/MOCR_audio"; //digitalocean space
 // var cTapeCdnRoot = parent.cMediaCdnRoot + '/MOCR_audio';
 
 var cWebCdnRoot = "";
--- _website/_webroot/13/navigator_dev.js
+++ _website/_webroot/13/navigator_dev.js
@@ -1,12 +1,12 @@
 trace("INIT: Loading index.js");
 var cStopCache = false;
 
-// var cTapeCdnRoot = 'https://media.apolloinrealtime.org/mp3';
-var cCdnRoot = 'https://keycdn.apolloinrealtime.org/mp3';
+var cTapeCdnRoot = "https://media.apolloinrealtime.org/mp3";
+// var cCdnRoot = 'https://keycdn.apolloinrealtime.org/mp3';
 // var cTapeCdnRoot = 'https://apollomedia.sfo2.cdn.digitaloceanspaces.com/mp3';
 // var cTapeCdnRoot = 'https://apollomedia.sfo2.digitaloceanspaces.com/mp3';
 
-var cWebCdnRoot = '';
+var cWebCdnRoot = "";
 // var cWebCdnRoot = 'https://apollort-26f5.kxcdn.com';
 
 var cYouTubeSDorHD = 0; //0 for SD  1 for HD
@@ -14,7 +14,7 @@ var cYouTubeSDorHD = 0; //0 for SD  1 for HD
 //constants
 var cMissionDurationSeconds = 518400;
 var cCountdownSeconds = 127048;
-var cDefaultStartTimeId = '-000102';
+var cDefaultStartTimeId = "-000102";
 var cLaunchDate = Date.parse("1970-04-11 19:13 -000");
 var cLaunchDateModern = Date.parse("2019-04-11 19:13 -000");
 var cCountdownStartDate = Date.parse("1970-04-10 7:55:50 -000"); //35 hours, 17 minutes, 10 seconds before launch
@@ -30,8 +30,8 @@ var gIntervalID = null;
 var gIntroInterval = null;
 var gApplicationReadyIntervalID = null;
 var YT = {
-    loading: 0,
-    loaded: 0
+  loading: 0,
+  loaded: 0,
 };
 
 //global flags
@@ -41,12 +41,12 @@ var gFontLoaderDelay = 3; //seconds
 var gSplashImageLoaded = false;
 var gMustInitNav = true;
 var gPlaybackState = "normal";
-var gLastTOCElement = '';
-var gLastTOCTimeId = '';
-var gLastCommentaryTimeId = '';
-var gLastUtteranceTimeId = '';
-var gLastTimeIdChecked = '';
-var gLastVideoSegmentDashboardHidden = '';
+var gLastTOCElement = "";
+var gLastTOCTimeId = "";
+var gLastCommentaryTimeId = "";
+var gLastUtteranceTimeId = "";
+var gLastTimeIdChecked = "";
+var gLastVideoSegmentDashboardHidden = "";
 var gUtteranceDisplayStartIndex;
 var gUtteranceDisplayEndIndex;
 var gCurrentHighlightedUtteranceIndex;
@@ -64,7 +64,7 @@ var gTapesActivityStartIndex = 0;
 var gTapesActivityRangeArray = [];
 
 //global mission state trackers
-var gCurrMissionTime = '';
+var gCurrMissionTime = "";
 var gCurrVideoStartSeconds = -9442; //countdown
 var gCurrVideoEndSeconds = 0;
 
@@ -92,108 +92,106 @@ var gGeoData = [];
 var gGeoCompendiumData = [];
 var gPaperData = [];
 
-
-$( document ).ready(function() {
-    setMissionTimeIncrement();
-    setAutoScrollPoller();
-
+$(document).ready(function () {
+  setMissionTimeIncrement();
+  setAutoScrollPoller();
 });
 
 function setMissionTimeIncrement() {
-    return window.setInterval(function () {
-        //trace("setIntroTimeUpdatePoller()");
-        gCurrMissionTime = secondsToTimeStr(timeStrToSeconds(gCurrMissionTime) + 1);
-    }, 1000);
+  return window.setInterval(function () {
+    //trace("setIntroTimeUpdatePoller()");
+    gCurrMissionTime = secondsToTimeStr(timeStrToSeconds(gCurrMissionTime) + 1);
+  }, 1000);
 }
 
 function setAutoScrollPoller() {
-    return window.setInterval(function () {
-        if (gMustInitNav) {
-            initNavigator(); //only init navigator after fonts have loaded to avoid mousex position bug
-            gMustInitNav = false;
-        }
-        if (gCurrMissionTime !== gLastTimeIdChecked) {
-            gLastTimeIdChecked = gCurrMissionTime;
-            //scroll nav cursor
-            if (!gMouseOnNavigator) {
-                drawTier1();
-                drawTier1NavBox(timeStrToSeconds(gCurrMissionTime));
-                drawTier2();
-                drawTier2NavBox(timeStrToSeconds(gCurrMissionTime));
-                drawTier3();
-            }
-            drawCursor(timeStrToSeconds(gCurrMissionTime));
-            paper.view.draw();
-        }
-    }, 500); //polling frequency in milliseconds
+  return window.setInterval(function () {
+    if (gMustInitNav) {
+      initNavigator(); //only init navigator after fonts have loaded to avoid mousex position bug
+      gMustInitNav = false;
+    }
+    if (gCurrMissionTime !== gLastTimeIdChecked) {
+      gLastTimeIdChecked = gCurrMissionTime;
+      //scroll nav cursor
+      if (!gMouseOnNavigator) {
+        drawTier1();
+        drawTier1NavBox(timeStrToSeconds(gCurrMissionTime));
+        drawTier2();
+        drawTier2NavBox(timeStrToSeconds(gCurrMissionTime));
+        drawTier3();
+      }
+      drawCursor(timeStrToSeconds(gCurrMissionTime));
+      paper.view.draw();
+    }
+  }, 500); //polling frequency in milliseconds
 }
 
 function populatePhotoGallery() {
-    //trace("populatePhotoGallery()");
-    redrawAll();
+  //trace("populatePhotoGallery()");
+  redrawAll();
 }
 
 function seekToTime() {
-    //trace("seekToTime()");
+  //trace("seekToTime()");
 }
 
 function padZeros(num, size) {
-    var s = num + "";
-    while (s.length < size) s = "0" + s;
-    return s;
+  var s = num + "";
+  while (s.length < size) s = "0" + s;
+  return s;
 }
 
 function secondsToTimeStr(totalSeconds) {
-    var hours = Math.abs(parseInt(totalSeconds / 3600));
-    var minutes = Math.abs(parseInt(totalSeconds / 60)) % 60 % 60;
-    var seconds = Math.abs(parseInt(totalSeconds)) % 60;
-    seconds = Math.floor(seconds);
-    var timeStr = padZeros(hours,3) + ":" + padZeros(minutes,2) + ":" + padZeros(seconds,2);
-    if (totalSeconds < 0) {
-        timeStr = "-" + timeStr.substr(1); //change timeStr to negative, replacing leading zero in hours with "-"
-    }
-    return timeStr;
+  var hours = Math.abs(parseInt(totalSeconds / 3600));
+  var minutes = (Math.abs(parseInt(totalSeconds / 60)) % 60) % 60;
+  var seconds = Math.abs(parseInt(totalSeconds)) % 60;
+  seconds = Math.floor(seconds);
+  var timeStr = padZeros(hours, 3) + ":" + padZeros(minutes, 2) + ":" + padZeros(seconds, 2);
+  if (totalSeconds < 0) {
+    timeStr = "-" + timeStr.substr(1); //change timeStr to negative, replacing leading zero in hours with "-"
+  }
+  return timeStr;
 }
 
 function secondsToTimeId(seconds) {
-    var timeId = secondsToTimeStr(seconds).split(":").join("");
-    return parseInt(timeId);
+  var timeId = secondsToTimeStr(seconds).split(":").join("");
+  return parseInt(timeId);
 }
 
 function timeIdToSeconds(timeId) {
-    var sign = timeId.substr(0,1);
-    var hours = parseInt(timeId.substr(0,3));
-    var minutes = parseInt(timeId.substr(3,2));
-    var seconds = parseInt(timeId.substr(5,2));
-    var signToggle = (sign == "-") ? -1 : 1;
+  var sign = timeId.substr(0, 1);
+  var hours = parseInt(timeId.substr(0, 3));
+  var minutes = parseInt(timeId.substr(3, 2));
+  var seconds = parseInt(timeId.substr(5, 2));
+  var signToggle = sign == "-" ? -1 : 1;
 
-    return signToggle * ((Math.abs(hours) * 60 * 60) + (minutes * 60) + seconds);
+  return signToggle * (Math.abs(hours) * 60 * 60 + minutes * 60 + seconds);
 }
 
 function timeIdToTimeStr(timeId) {
-    return timeId.substr(0,3) + ":" + timeId.substr(3,2) + ":" + timeId.substr(5,2);
+  return timeId.substr(0, 3) + ":" + timeId.substr(3, 2) + ":" + timeId.substr(5, 2);
 }
 
 function timeStrToTimeId(timeStr) {
-    return timeStr.split(":").join("");
+  return timeStr.split(":").join("");
 }
 
 function timeStrToSeconds(timeStr) {
-    var sign = timeStr.substr(0,1);
-    var hours = parseInt(timeStr.substr(0,3));
-    var minutes = parseInt(timeStr.substr(4,2));
-    var seconds = parseInt(timeStr.substr(7,2));
-    var signToggle = (sign == "-") ? -1 : 1;
-    return Math.round(signToggle * ((Math.abs(hours) * 60 * 60) + (minutes * 60) + seconds));
+  var sign = timeStr.substr(0, 1);
+  var hours = parseInt(timeStr.substr(0, 3));
+  var minutes = parseInt(timeStr.substr(4, 2));
+  var seconds = parseInt(timeStr.substr(7, 2));
+  var signToggle = sign == "-" ? -1 : 1;
+  return Math.round(signToggle * (Math.abs(hours) * 60 * 60 + minutes * 60 + seconds));
 }
 
 function trace(str) {
-    var debug = true;
-    if (debug === true) {
-        try {
-            console.log(str);
-        } catch (e) {
-            //no console, no trace
-        }
+  var debug = true;
+  if (debug === true) {
+    try {
+      console.log(str);
+    } catch (e) {
+      //no console, no trace
     }
+  }
 }
